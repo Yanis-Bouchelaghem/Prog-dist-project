f4cc9a1cc2a84e52356febfc98a7c1adfdfaf5df
Yanis Bouchelaghem <bouchelaghemyanis7@gmail.com>
2024-05-20 00:20:56 +0200

    implemented gateway

--- backend/database/db_engine.py
+++ backend/database/db_engine.py
@@ -1,10 +1,13 @@
 from sqlmodel import create_engine
 import os
-
+import time
+from sqlalchemy.exc import SQLAlchemyError
 POSTGRES_USER = os.environ['POSTGRES_USER']
 POSTGRES_PASSWORD = os.environ['POSTGRES_PASSWORD']
 POSTGRES_DB = os.environ['POSTGRES_DB']
 ENGINE_ECHO = os.environ['ENGINE_ECHO'] == 'TRUE'
 
+
+time.sleep(20)
 database_engine = create_engine(f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@postgres:5432/{POSTGRES_DB}",
-                                echo=ENGINE_ECHO)
+                    echo=ENGINE_ECHO)
